578cffb2e5e2caaeb3c13e9c3f78495f8aa11b0a
jeev86399 <nnani86399@gmail.com>
2024-12-13 10:24:03 +0530

    Add files via upload

---
+++ arrlong56.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arrlong56{
+    public static void main(String args[]){
+        long ans=90;
+        Scanner s=new Scanner(System.in);
+        long b=s.nextLong();
+        System.out.println(ans);
+        System.out.println(b);
+    }
+}
