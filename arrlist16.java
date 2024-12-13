46d2ba1da75e79e28a6c1fd7fbaac040333b585e
jeev86399 <nnani86399@gmail.com>
2024-12-13 09:40:48 +0530

    Add files via upload

---
+++ arrlist16.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arrlist16{
+    public static void main(String args[]){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(345);
+        a.add(909);
+        a.add(456);
+        System.out.println(a);
+    }
+}
